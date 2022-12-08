c984814e5b6c8fc8aa9dfe75f1390ff756d790a7
RezademploSyah <reza_syah_2022@abbs.alabidin.sch.id>
2022-12-08 19:48:04 +0700

    first commit

---
+++ tilticoba.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.cobajs').tilt({
+
+      glare: true,
+      maxGlare: .5,
+  })
+
+  $('.sm-tilt').tilt({
+    scale: 0.9,
+      glare: true,
+      maxGlare: .9
+  
+  })
+  
+})
